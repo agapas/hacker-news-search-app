e28eda554a1412e414472cef5ff629ed59052858
Agnieszka Pas <12623793+agapas@users.noreply.github.com>
2022-02-13 15:10:01 +0000

    Fetched query data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
+  const [results, setResults] = useState([]);
+
   useEffect(() => {
-    axios.get("http://hn.algolia.com/api/v1/search_by_date?=reacthooks")
-      .then(response => console.log(response.data))
+    axios.get("http://hn.algolia.com/api/v1/search_by_date?query=reacthooks&tags=story")
+      .then(response => {
+        console.log(response.data);
+        setResults(response.data.hits);
+      })
       .catch((error) => {
         console.error('Error:', error);
       });
-  });
+  }, []);
 
   return (
-    <div className="App">App</div>
+    <div className="App">
+      <ul>
+        {results.map(result => (
+          <li key={result.objectID}>
+            <a href={result.url}>{result.title}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 

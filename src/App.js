cece21685e22b6bfc9fee61d891f201172dfcb93
Agnieszka Pas <12623793+agapas@users.noreply.github.com>
2022-02-13 17:07:03 +0000

    Added search and clear functionality

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 
 function App() {
   const [results, setResults] = useState([]);
+  const [query, setQuery] = useState("react hooks");
+  const searchInputRef = useRef();
+  
+  useEffect(() => loadData(), []);
 
-  useEffect(() => {
-    axios.get("http://hn.algolia.com/api/v1/search_by_date?query=reacthooks&tags=story")
-      .then(response => {
-        console.log(response.data);
-        setResults(response.data.hits);
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
-  }, []);
+  const loadData = async () => {
+    const response = await axios.get(
+      `http://hn.algolia.com/api/v1/search_by_date?query=${query}&tags=story`
+    );
+    setResults(response.data.hits);
+  }
+
+  const handleSearch = event => {
+    event.preventDefault();
+    loadData();
+  }
+
+  const handleClearSearch = () => {
+    setQuery("");
+    searchInputRef.current.focus();
+  }
 
   return (
     <div className="App">
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          ref={searchInputRef}
+          value={query}
+          onChange={event => setQuery(event.target.value)}
+        />
+        <button type="submit">Search</button>
+        <button type="button" onClick={handleClearSearch}>Clear</button>
+      </form>
       <ul>
         {results.map(result => (
           <li key={result.objectID}>

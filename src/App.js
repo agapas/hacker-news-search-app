b20cf27848a3d30b94605d1f3f88328e0adaa7e2
Agnieszka Pas <12623793+agapas@users.noreply.github.com>
2022-02-13 14:46:07 +0000

    Added axios package

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  useEffect(() => {
+    axios.get("http://hn.algolia.com/api/v1/search_by_date?=reacthooks")
+      .then(response => console.log(response.data))
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  });
+
   return (
     <div className="App">App</div>
   );

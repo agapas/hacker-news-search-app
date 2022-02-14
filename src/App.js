d6dd0635d7ac739de56801b71c37df2b6c82bc51
Agnieszka Pas <12623793+agapas@users.noreply.github.com>
2022-02-14 18:49:07 +0000

    Added responsive styles using tailwind

--- src/App.js
+++ src/App.js
@@ -37,33 +37,46 @@ function App() {
     searchInputRef.current.focus();
   }
 
+  const resultTextColor = loading ? "text-gray-500" : error ? "text-red-500" : "";
+  const resultText = loading ? "Loading..." : error ?? "Results:";
+
   return (
-    <div className="App">
-      <form onSubmit={handleSearch}>
+    <div
+      className="container max-w-full md:max-w-xl mx-auto p-6 md:my-8 bg-yellow-50 shadow-lg rounded"
+    >
+      <h1 className="text-gray-500 text-3xl mb-2 font-thin">Hacker News</h1>
+      <form className="w-full md:flex" onSubmit={handleSearch}>
         <input
+          className="w-full md:w-3/5 border rounded px-2 py-1 mr-2 mb-2"
           type="text"
           ref={searchInputRef}
           value={query}
           onChange={event => setQuery(event.target.value)}
         />
-        <button type="submit">Search</button>
-        <button type="button" onClick={handleClearSearch}>Clear</button>
+        <button
+          className="w-full md:w-1/5 rounded bg-yellow-400 hover:bg-yellow-500 px-2 py-1 mr-2 mb-2"
+          type="submit"
+        >Search</button>
+        <button
+          className="w-full md:w-1/5 rounded bg-blue-400 hover:bg-blue-500 px-2 py-1 mb-2"
+          type="button"
+          onClick={handleClearSearch}
+        >Clear</button>
       </form>
 
-      {loading ? (
-        <div>Loading...</div>
-      ) : (error ? (
-        <div>{error}</div>
-        ) : (
-          <ul>
-            {results.map(result => (
-              <li key={result.objectID}>
-                <a href={result.url}>{result.title}</a>
-              </li>
-            ))}
-          </ul>
-        )
-      )}
+      <div className={`${resultTextColor} text-lg mt-2 mb-1`}>{resultText}</div>
+
+      {!loading && !error &&
+        <ul className="list-reset leading-normal">
+        {results.map(result => (
+          <li key={result.objectID}>
+            <a
+              className={result.url ? "text-blue-600 hover:text-blue-700" : ""}
+              href={result.url}
+            >{result.title}</a>
+          </li>))}
+        </ul>
+      }
     </div>
   );
 }
